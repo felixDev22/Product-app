05b5dfca0b6996673a5417069275e5ce04efece1
felixDev22 <felixnzomo@gmail.com>
2024-02-13 23:35:34 +0300

    Intall faker to fetch data, create context folder/file

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Context from './components/context/Context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>,
 );
