6d30ea92991b3c91060fe4f289cce37491e675b8
felixDev22 <felixnzomo@gmail.com>
2024-02-15 23:53:47 +0300

    Use axios to import products

--- src/components/context/Context.jsx
+++ src/components/context/Context.jsx
@@ -1,9 +1,27 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Cart = createContext();
 
 const Context = ({ children }) => {
-  return <Cart.Provider>{children}</Cart.Provider>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
+  return <Cart.Provider value={products}>{children}</Cart.Provider>;
 };
 
 export default Context;
