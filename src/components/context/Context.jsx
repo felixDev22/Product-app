5b7a265212ceaf34a5c6d2071efe763a1d51af2b
felixDev22 <felixnzomo@gmail.com>
2024-02-16 23:39:52 +0300

    Rearrange the code for clarity

--- src/components/context/Context.jsx
+++ src/components/context/Context.jsx
@@ -1,8 +1,19 @@
-import React, { createContext, useState, useEffect, useReducer } from 'react';
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+} from 'react';
 import axios from 'axios';
 import { cartReducer } from './cartReducer';
+import { productReducer } from './productReducer';
 
 const Cart = createContext();
+const [state, dispatch] = useReducer(cartReducer, {
+  products: [],
+  cart: [],
+});
 
 const Context = ({ children }) => {
   const [products, setProducts] = useState([]);
@@ -11,6 +22,7 @@ const Context = ({ children }) => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get('https://dummyjson.com/products');
+        dispatch({ type: 'SET_PRODUCT', payload: response.data });
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
@@ -20,12 +32,13 @@ const Context = ({ children }) => {
     fetchProducts();
   }, []);
 
-  const [state, dispatch] = useReducer(cartReducer, {
-    products: products,
-    cart: [],
-  });
+  console.log(products);
 
   return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
 };
 
 export default Context;
+
+export const CartState = () => {
+  return useContext(Cart);
+};
