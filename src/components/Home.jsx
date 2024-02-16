5427f39f73e9444536bde720faf64c29a695c0fd
felixDev22 <felixnzomo@gmail.com>
2024-02-16 23:39:00 +0300

    Update hoem to render the products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { CartState } from './context/Context';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { state } = CartState();
+
+  console.log('Type ofe state.prpducts:', typeof state.products);
+
+  if (state.products.length === 0) {
+    return <div>Loading...</div>;
+  }
+  console.log(state.products);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {state.products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
