5301ee5238817f1b30632aab19003e66b172d1c8
felixDev22 <felixnzomo@gmail.com>
2024-02-21 23:50:49 +0300

    Add rating function for the products

--- src/components/SideFilter.jsx
+++ src/components/SideFilter.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, FormCheck } from 'react-bootstrap';
+import Rating from './Rating';
 
 const SideFilter = () => {
+  const [rate, setRate] = useState(2);
+
   return (
     <div className="filter-container">
       <span>
@@ -40,10 +43,14 @@ const SideFilter = () => {
           id="{inline-4}"
         />
       </span>
-      {/* <span>
+      <span>
         <label style={{ paddingRight: 10 }}>Rating:</label>
-        <Rating rating={rating} style={{ cursor: 'pointer' }} />
-      </span> */}
+        <Rating
+          rating={rate}
+          onClick={(i) => setRate(i + 1)}
+          style={{ cursor: 'pointer' }}
+        />
+      </span>
       <Button variant="light" className="btn">
         clear
       </Button>
