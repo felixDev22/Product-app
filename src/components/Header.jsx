da06fbd27737befc1594855b265ac3cbdd6104d1
felixDev22 <felixnzomo@gmail.com>
2024-03-04 23:47:20 +0300

    Add a counter for cart, to keep track of the products

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,13 @@ import {
 } from 'react-bootstrap';
 import { FaCartShopping } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import { CartState } from './context/Context';
 
 const Header = () => {
+  const {
+    state: { cart },
+  } = CartState();
+
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 70 }}>
       <Container>
@@ -29,7 +34,7 @@ const Header = () => {
             id="dropdown-menu-align-end">
             <Dropdown.Toggle variant="success">
               <FaCartShopping color="white" fontSize="20px" />
-              <Badge>{10}</Badge>
+              <Badge>{cart.length}</Badge>
             </Dropdown.Toggle>
             <Dropdown.Menu style={{ minWidth: 370 }}>
               <span style={{ padding: 10 }}>Cart is Emty!</span>
